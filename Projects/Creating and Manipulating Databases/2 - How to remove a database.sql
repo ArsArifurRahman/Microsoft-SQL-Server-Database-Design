e20eacffba2ab80dd05143bcb2079946d023e9d1
ArsArifurRahman <ars.ict.developer@gmail.com>
2024-03-17 00:17:47 +0600

    Modified

---
+++ Projects/Creating and Manipulating Databases/2 - How to remove a database.sql
@@ -0,0 +1,5 @@
+use master;
+
+--CREATE DATABASE DatabaseToBeDeleted;
+
+DROP DATABASE DatabaseToBeDeleted;
---
+++ Projects/Creating and Manipulating Databases/3 - How to change database name.sql
@@ -0,0 +1,5 @@
+use master;
+
+CREATE DATABASE DatabaseToBeRenamed
+
+ALTER DATABASE DatabaseToBeRenamed MODIFY NAME = RenamedDatabase
